e3160a630e1525c9d9a60b4f05368ade4f1edc5e
sam-1wdv1 <65342540+sam-1wdv1@users.noreply.github.com>
2020-10-13 16:12:04 +0200

    Add files via upload

---
+++ API/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<title>api-TEST</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	</head>
+	<body>
+        <script>
+            fetch('product/read_all.php')
+            .then(response => response.json())
+            .then(data => {
+                for(let i=0; i<data.length; i++) {
+                    let div = document.createElement('div');
+                    let textnode = document.createTextNode(naam);
+                    div.appendChild(textnode);
+                    document.body.appendChild(div);
+                }
+            })
+            .catch(error => console.log('error is', error));
+        </script>
+    </body>
+</html>
